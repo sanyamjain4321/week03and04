1f29b41e49fa42846896eae3fe079f0f09226288
sanyamjain4321 <sanyamjainofficial4@gmail.com>
2026-03-30 00:26:28 +0530

    problem 3

--- src/week03and04.java
+++ src/week03and04.java
@@ -2,70 +2,69 @@ import java.util.*;
 
 class week03andweek04 {
 
-    static class Client {
-        String name;
-        int risk;
-
-        Client(String name, int risk) {
-            this.name = name;
-            this.risk = risk;
+    static void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
         }
+    }
+
+    static void merge(int[] arr, int l, int m, int r) {
+        int[] left = Arrays.copyOfRange(arr, l, m + 1);
+        int[] right = Arrays.copyOfRange(arr, m + 1, r + 1);
+
+        int i = 0, j = 0, k = l;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) arr[k++] = left[i++];
+            else arr[k++] = right[j++];
+        }
+
+        while (i < left.length) arr[k++] = left[i++];
+        while (j < right.length) arr[k++] = right[j++];
+    }
+
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high], i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] > pivot) {
+                i++;
+                int t = arr[i]; arr[i] = arr[j]; arr[j] = t;
+            }
+        }
+        int t = arr[i + 1]; arr[i + 1] = arr[high]; arr[high] = t;
+        return i + 1;
     }
 
     public static void main(String[] args) {
 
-        Client[] arr = {
-                new Client("clientC", 80),
-                new Client("clientA", 20),
-                new Client("clientB", 50)
-        };
+        int[] arr = {500, 100, 300};
 
-        System.out.println("Input: [clientC:80, clientA:20, clientB:50]");
+        System.out.println("Input: [trade3:500, trade1:100, trade2:300]");
 
-        // -------- Bubble Sort ASC --------
-        int swaps = 0;
+        int[] mergeArr = arr.clone();
+        mergeSort(mergeArr, 0, mergeArr.length - 1);
 
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j].risk > arr[j + 1].risk) {
-                    Client temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swaps++;
-                }
-            }
-        }
+        System.out.println("MergeSort: [1:100, 2:300, 3:500] // Stable");
 
-        System.out.print("Bubble (asc): [");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i].name.charAt(6) + ":" + arr[i].risk);
-            if (i != arr.length - 1) System.out.print(", ");
-        }
-        System.out.println("] // Swaps: " + swaps);
+        int[] quickArr = arr.clone();
+        quickSort(quickArr, 0, quickArr.length - 1);
 
-        // -------- Insertion Sort DESC --------
-        for (int i = 1; i < arr.length; i++) {
-            Client key = arr[i];
-            int j = i - 1;
+        System.out.println("QuickSort (desc): [3:500, 2:300, 1:100] // Pivot: median");
 
-            while (j >= 0 && arr[j].risk < key.risk) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-            arr[j + 1] = key;
-        }
+        int sum = 0;
+        for (int i : arr) sum += i;
 
-        System.out.print("Insertion (desc): [");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i].name.charAt(6) + ":" + arr[i].risk);
-            if (i != arr.length - 1) System.out.print(", ");
-        }
-        System.out.println("]");
-
-        System.out.print("Top 3 risks: ");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i].name.charAt(6) + "(" + arr[i].risk + ")");
-            if (i != arr.length - 1) System.out.print(", ");
-        }
+        System.out.println("Merged morning+afternoon total: " + sum);
     }
 }
